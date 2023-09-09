4e9faf06fe69807ff7de32232014095c6d43b150
Peter Halliday <pghalliday@gmail.com>
2023-09-09 05:22:48 +0200

    output coloring

--- tools/burp/src/burp/cli/__init__.py
+++ tools/burp/src/burp/cli/__init__.py
@@ -15,14 +15,38 @@ from burp.params import BUILD, CLEAN, FULL_CLEAN, FLASH, MONITOR
 _LOGGER = logging.getLogger(__name__)
 
 
+class StdOutFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(name)s: %(levelname)s: %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 def _setup_logger(log_level: str, log_file: Path) -> None:
     logger = logging.getLogger()
     logger.setLevel(log_level)
     log_file.parent.mkdir(parents=True, exist_ok=True)
     file_handler = logging.FileHandler(log_file)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(name)s: %(message)s'))
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s: %(name)s: %(levelname)s: %(message)s (%(filename)s:%(lineno)d)'
+    ))
     stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setFormatter(logging.Formatter('%(levelname)s: %(name)s: %(message)s'))
+    stdout_handler.setFormatter(StdOutFormatter())
     logger.addHandler(file_handler)
     logger.addHandler(stdout_handler)
 
