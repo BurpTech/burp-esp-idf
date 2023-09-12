5b13da2f49f4f3623d500077a8522455a307e14c
Peter Halliday <pghalliday@gmail.com>
2023-09-12 21:10:02 +0200

    buttons to trigger commands :)

--- tools/burp-web-app/src/Flash.tsx
+++ tools/burp-web-app/src/Flash.tsx
@@ -1,4 +1,4 @@
-import {Container, Grid} from "@mui/material";
+import {Button, Container, Grid, Stack} from "@mui/material";
 import * as React from "react";
 import FlashGridItem from "./FlashGridItem";
 import {FlashTerminal} from "./lib/terminals/command-terminal";
@@ -10,8 +10,20 @@ interface FlashProps {
 export default function Flash(props: FlashProps) {
   const {flashTerminals} = props;
 
+  function doFlash() {
+    fetch('/flash').then(response => {
+      console.log(response.status);
+      response.json().then(data => {
+        console.log(data);
+      });
+    });
+  }
+
   return (
     <Container maxWidth={false}>
+      <Stack direction="row" spacing={2} paddingY={2}>
+        <Button variant="contained" onClick={doFlash}>Flash</Button>
+      </Stack>
       <Grid container spacing={2} height="100%">
         {flashTerminals.map(flashTerminal => (
           <FlashGridItem key={flashTerminal.device.name} flashTerminal={flashTerminal}/>
